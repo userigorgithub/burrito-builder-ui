bdca94dd7edffa841c6962f2caa6d2dee38ab914
Igor Decess <i.decess@comcast.net>
2022-06-14 12:03:25 -0600

    Add cypress test for submitting an order third test

--- cypress/e2e/home_page_spec.cy.js
+++ cypress/e2e/home_page_spec.cy.js
@@ -5,7 +5,7 @@ describe("Burrito Builder home page", () => {
     cy.visit("http://localhost:3000")
   });
 
-  it('should be able to visit the home page, see title, see form page, and see current orders', () => {
+  it('should be able to visit the home page, see title, see form, and see current orders', () => {
     cy.get("h1")
       .should("contain", "Burrito Builder")
     cy.get("form")
@@ -20,11 +20,32 @@ describe("Burrito Builder home page", () => {
     cy.get("input[name='name']")
       .type("Igor")
       .should("have.value", "Igor")
-    cy.get("button[name='steak']").click()
+    cy.get("button[name='steak']")
+      .click()
     cy.get("p")
       .should("contain", "steak")
   });
 
+  it('should be able to submit a form and see it being displayed', () => {
+    cy.intercept("POST", "http://localhost:3001/api/v1/orders", {
+      statusCode: 201,
+      body: {
+        name: "Igor",
+        ingredients: ["steak"]
+      }
+    })
+    cy.get("input[name='name']")
+      .type("Igor")
+    cy.get("button[name='steak']")
+      .click()
+    cy.get(".submit-btn")
+      .click()
+    cy.get(".orders")
+    cy.get(".order")
+      .contains("Igor")
+    cy.get(".order")
+      .should("contain", "steak")
+  });
 
 
 
--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -63,7 +63,7 @@ class OrderForm extends Component {
 
         <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
+        <button className="submit-btn" onClick={e => this.handleSubmit(e)}>
           Submit Order
         </button>
       </form>
