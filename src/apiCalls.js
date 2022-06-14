212fac129a0e9f8077f2a70b3c5b243b40444d63
Igor Decess <i.decess@comcast.net>
2022-06-14 09:57:02 -0600

    Add POST function in apiCalls to post orders

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,14 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrders = (newOrder) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newOrder),
+  })
+  .then(response => response.json())
+}
