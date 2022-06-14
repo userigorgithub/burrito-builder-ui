d5480cf1cc1fcb04741b7d6dd7b55774b04005b2
Igor Decess <i.decess@comcast.net>
2022-06-14 09:41:33 -0600

    Add .then with data within componentDidMount method for orders to be fetched

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 
   componentDidMount() {
     getOrders()
+      .then(data => this.setState({ orders: data.orders }))
       .catch(err => console.error('Error fetching:', err));
   }
 
