f6c6fce61578c3d67f66d831ea456cc9adc71225
Igor Decess <i.decess@comcast.net>
2022-06-14 11:29:40 -0600

    Add cypress testing for form and order first test

--- cypress/e2e/home_page_spec.cy.js
+++ cypress/e2e/home_page_spec.cy.js
@@ -5,10 +5,22 @@ describe('Burrito Builder home page', () => {
     cy.visit("http://localhost:3000")
   });
 
-  it('should be able to visit the home page and see title', () => {
+  it('should be able to visit the home page, see title, see form page, and see current orders', () => {
     cy.get("h1")
       .should("contain", 'Burrito Builder')
+    cy.get("form")
+      .should("contain", 'Submit Order')
+    cy.get(".orders")
+    cy.get(".order")
+      .should("have.length", 2)
+  });
+
+  it.skip('should be able to fill out inputs of form', () => {
+    
 
   });
+
+
+
 
 });
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -16,7 +16,7 @@ const Orders = props => {
   });
 
   return (
-    <section>
+    <section className="orders">
       { orderEls.length ? orderEls : <p>No orders yet!</p> }
     </section>
   )
