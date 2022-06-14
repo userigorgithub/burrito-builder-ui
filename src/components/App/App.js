0b0e9ae8e996bffe2757299fe2135e179acee999
Igor Decess <i.decess@comcast.net>
2022-06-14 10:07:33 -0600

    Add addOrder function in App to add order for post

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import {getOrders, postOrders} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
@@ -17,6 +17,12 @@ class App extends Component {
       .then(data => this.setState({ orders: data.orders }))
       .catch(err => console.error('Error fetching:', err));
   }
+
+  addOrder = (newOrder) => {
+    postOrders(newOrder)
+      .then(data => this.setState({ orders: [...this.state.orders, data] }))
+  }
+
 
   render() {
     return (
